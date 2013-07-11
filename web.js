73530db9f0dfa9060a7e7e2198eccc104ec922f8
Chad <cwb2123@columbia.edu>
2013-07-11 19:32:07 +0000

    added file read

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var fs = require('fs');
+var indexFile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var buffer;
+	fs.readFileSync(indexFile,function (err,data){
+	    if(err) throw err;
+	    buffer  = new Buffer(data);
+	});
+	
+
+    response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
